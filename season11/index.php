bf6aef695c857206a1a5d3fe329eae3fb8172265
David J. Smario <david.smario@herodigital.com>
2019-08-09 16:33:16 -0700

    Season 12!

---
+++ season11/index.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="../css/main.css" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400,600,700,800' rel='stylesheet' type='text/css'>
+	<title>
+		Deaf Bay Area Poker Series
+	</title>
+</head>
+
+<body>
+
+	<header>
+		<section class="page clearfix">
+			<a href="/">
+				<div id="logo"></div>
+			</a>
+			<nav id="main">
+				<a href="downloads/season11/baps-season11-event6.pdf" target="_blank">Season 11 - Event 6</a>
+				<a href="downloads/season11/baps-season11-points.pdf" target="_blank">Season 11 Points</a>
+				<!--<a href="downloads/guidelines11.pdf" target="_blank">Guidelines - Season 11</a>-->
+				<a href="http://eepurl.com/n5HGH" target="_blank">Subscribe to BAPS</a>
+			</nav>
+		</section>
+	</header>
+	<section id="bottom">
+		<section class="page clearfix">
+            <br>
+            <br>
+            <h2>Tournament Schedule - Season 12, 2019-20</h2>
+            <br>
+            <span class="next">Event 1 – August 10, 2019</span> <!-- if completed, enter class="completed" -->
+            <br>
+            <span class="">Event 2 - September 21, 2019 </span>
+            <br>
+            <span class="">SFDC Tourney (Event 3) – October 5, 2019</span>
+            <br>
+            <span class="">Event 4 - November 9, 2019</span>
+            <br>
+            <span class="">Event 5 – January 11, 2020</span>
+            <br>
+            <span class="">Event 6 – February 8, 2020</span>
+            <br>
+            <span class="">Event 7 – March 14, 2020</span>
+            <br>
+            <span class="">Event 8 – April 11, 2020</span>
+            <br>
+            <span class="">Event 9 – May 2, 2020</span>
+            <br>
+            <br>
+            <h2>"Freeroll" ($20 entry fee)</h2>
+            <span class="">June 13, 2020</span>
+            <br>
+		</section>
+	</section>
+	<section id="top">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11champ.png" width="300" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season #11 Champion!</h1>
+			<h1>Adam Jarashow</h1>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="bottom">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11event6.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season 11, Event 6 Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Tony Cordero
+					<br />
+					2nd - Clark Brooke
+					<br />
+					3rd - Ty Kovacs
+					<br />
+					4th - Mike Scharnow
+					<br />
+					5th - Darla Holmes
+					<br />
+					6th - David Seto
+					<br />
+					7th - Sulghi Hong
+					<br />
+					8th - Brotrae Bullock
+					<br />
+					9th - Artye Morriese
+				</div>
+                Total players: 44
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="top">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11event5.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season 11, Event 5 Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Tony Cordero
+					<br />
+					2nd - Pat Woo
+					<br />
+					3rd - Sulghi Hong
+					<br />
+					4th - Jerry Grigsby
+					<br />
+					5th - Eric Kunze
+					<br />
+					6th - Richard Apple
+					<br />
+					7th - Chris DeSouza
+					<br />
+					8th - Jason Hom
+					<br />
+					9th - Dominex Kovacs
+				</div>
+                Total players: 44
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="bottom">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11event4.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season 11, Event 4 Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Adam Jarashow
+					<br />
+					2nd - Don Lee Hanaumi
+					<br />
+					3rd - Matt Cerar
+					<br />
+					4th - Pat Woo
+					<br />
+					5th - Mike Scharnow
+					<br />
+					6th - Ted Baldwin
+					<br />
+					7th - David Smario
+					<br />
+					8th - Julia Hong
+					<br />
+					9th - Joshua Weinstock
+				</div>
+                Total players: 39
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="top">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11event3.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season 11, Event 3 Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Stanley Matsumoto
+					<br />
+					2nd - John Brooks
+					<br />
+					3rd - Clark Brooke
+					<br />
+					4th - David Smario
+					<br />
+					5th - Jerry Grigsby
+					<br />
+					6th - Darla Holmes
+					<br />
+					7th - Eric Kunze
+					<br />
+					8th - Valeriy Kutsov
+					<br />
+					9th - Don Lee Hanaumi
+				</div>
+                Total players: 36
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="bottom">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11sfdc.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to SFDC Tourney Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Jason Hom
+					<br />
+					2nd - Matt Cerar
+					<br />
+					3rd - Scott O'Donnell 
+					<br />
+					4th - David Seto
+					<br />
+					5th - Ty Kovacs
+					<br />
+					6th - David Weiss
+					<br />
+					7th - Joshua Weinstock
+					<br />
+					8th - Adam Jarashow
+					<br />
+					9th - David Smario
+				</div>
+                Total players: 47
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="top">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11event2.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season 11, Event 2 Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Adam Jarashow
+					<br />
+					2nd - Matt Cerar
+					<br />
+					3rd - Eric Kunze
+					<br />
+					4th - Pat Woo
+					<br />
+					5th - Mike Scharnow
+					<br />
+					6th - David Smario
+					<br />
+					7th - Mark Farr
+					<br />
+					8th - Jason Dietz
+					<br />
+					9th - Tan Nguyen
+				</div>
+                Total players: 38 
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="bottom">
+		<section class="page clearfix">
+			<br />
+			<br />
+			<img src="images/season-11/season11event1.png" width="400" style="float:right; margin: 0 0 10px 0;" />
+			<h1>Congratulations to Season 11, Event 1 Winners</h1>
+			<p>
+				<div class="left schedule">
+					1st - Douglas Fischer
+					<br />
+					2nd - David Weiss
+					<br />
+					3rd - Clark Brooke
+					<br />
+					4th - Jerry Grigsby
+					<br />
+					5th - Damira Bechthold
+					<br />
+					6th - Alison Amable
+					<br />
+					7th - Don Lee Hanaumi
+					<br />
+					8th - Joey Baer
+					<br />
+					9th - Bob Ellis
+				</div>
+                Total players: 46
+			</p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="top">
+		<section class="page clearfix">
+			<br />
+			<br />
+				<h1>Get ready for Season 11, August 2018 to June 2019</h1>
+				<h2>Event 2 - September 8th!</h2>
+                <p>
+                    &bullet; Doors will open at 4:30PM for registration.
+    				<br />
+                    &bullet; There will be $10,000 chips for <b>ALL PLAYERS</b> between 4:30PM and 5:00PM.
+    				<br />
+                    &bullet; There will be $9,000 chips for prepaid players after 5:00PM, up to 3 rounds. If no show after 3 rounds, the chips will be taken away.
+    				<br />
+                    &bullet; Cards will be dealt at 5:00PM sharp.
+    				<br />
+                    &bullet; $40 entry fee.
+    				<br /><br />
+                    <b>Location for BAPS for Season 9</b>
+    				<br />
+                    Fremont Oak Gardens (FOG)
+                    <br />
+                    2681 Driscoll Road, Fremont, CA
+                    <br />
+                    <a href="https://www.google.com/maps/preview?q=2681+Driscoll+Road,+Fremont,+CA" target="_blank">MAP</a>
+    				<br /><br />
+                    &bullet; **DO NOT bring your chair. There are plenty of chairs at FOG.**
+                </p>
+				<p>
+			<br />
+			<br />
+		</section>
+	</section>
+	<section id="bottom">
+		<section class="page clearfix">
+			<div class="box 1">
+				<h2>Welcome</h2>
+
+				<p>
+					BAPS is organized exclusively for the Deaf poker community and is open to any player in the ASL community who would like to participate.
+				</p>
+				<p>
+					You can like us on our facebook page and/or subscribe to our annoucements.
+				</p>
+
+			</div>
+			<div class="box 2">
+				<h2>News</h2>
+				<p>
+					BAPS is now organized by a committee: Alison Amable, Darla Holmes, Elizabeth Grigsby, David Seto, David Smario, Eric Kunze, Jaime Jackerson, Jerry Grigsby
+				</p>
+				<p>Good luck!</p>
+			</div>
+            <p>&nbsp;</p>
+			<div class="box 3">
+				<h2>Tournament Schedule - Season 10, 2018-19</h2>
+
+				<span class="completed">Event 1 – August 11th</span> <!-- if completed, enter class="completed" -->
+				<br>
+				<span class="completed">Event 2 - September 8th </span>
+				<br>
+				<span class="completed">SFDC Tourney – October 6th</span> <span class=""><a href="http://www.sfdeafclub.org/2018/09/13/sfdc-poker-2018-oct-6" target="_blank">(flyer)</a></span>
+				<br>
+				<span class="completed">Event 3 - November 10th</span>
+				<br>
+				<span class="completed">Event 4 – January 12th</span>
+				<br>
+				<span class="completed">Event 5 – February 9th</span>
+				<br>
+				<span class="completed">Event 6 – March 9th</span>
+				<br>
+				<br>
+				<h2>"Freeroll" ($20 entry fee)</h2>
+				<span class="completed">June 8th</span>
+				<br>
+
+			</div>
+
+		</section>
+
+	</section>
+
+	<footer>
+		<section class="page" style="padding-top: 20px;">
+
+			<nav style="width: 100%; margin: 0px auto; text-align: center; padding-bottom: 0px;">
+                <p>
+    				<a href="season10">View Season 10</a>
+    				<a href="season9">View Season 9</a>
+    				<a href="season8">View Season 8</a>
+    				<a href="season7">View Season 7</a>
+    				<a href="season6">View Season 6</a>
+    				<a href="season5">View Season 5</a>
+                </p>
+                <p>
+    				<a href="downloads/cash_distribution.pdf" target="_blank">Cash Distribution</a>
+    				<a href="downloads/BAPSchampions.pdf" target="_blank">BAPS Champions</a>
+    				<a href="downloads/MostBAPSTourneyWins.pdf" target="_blank">Most BAPS Tourney Wins</a>
+    				<a href="downloads/baps_history.pdf" target="_blank">Player History</a>
+    				<a href="http://eepurl.com/n5HGH" target="_blank">Subscribe to BAPS</a>
+                </p>
+			</nav>
+            <p style="clear:both;">&nbsp;</p>
+
+			<div>&copy; Deaf Bay Area Poker Series - 2013&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/groups/bayareapokerseries/" target="_blank"><img src="images/Facebook_icon.png" border="0" width="25" /></a></div>
+			<br />
+		</section>
+	</footer>
+
+</body>
+</html>
