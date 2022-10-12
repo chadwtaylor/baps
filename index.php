c7931524eba4df2cf660d173757d09bc7aed9841
David J. Smario <david.smario@herodigital.com>
2022-10-12 14:13:46 -0700

    Season 14 Event 2 & 3

--- index.php
+++ index.php
@@ -2,109 +2,188 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" href="css/main.css?v=1.1" type="text/css" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400,600,700,800' rel='stylesheet' type='text/css'>
-	<title>
-		Deaf Bay Area Poker Series
-	</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="css/main.css?v=1.1" type="text/css" />
+    <link
+        href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,800italic,400,600,700,800'
+        rel='stylesheet' type='text/css'>
+    <title>
+        Deaf Bay Area Poker Series
+    </title>
 </head>
 
 <body>
-	<header>
-		<section class="page clearfix">
-			<a href="/">
-				<div id="logo"></div>
-			</a>
-			<nav id="main">
-				<!--
+    <header>
+        <section class="page clearfix">
+            <a href="/">
+                <div id="logo"></div>
+            </a>
+            <nav id="main">
+                <!--
 				<a href="downloads/BAPS-Guidelines-Season14.pdf" target="_blank">Guidelines - Season 14</a>
                 -->
-				<a href="downloads/season14/baps-season14-event1.pdf" target="_blank">Season 14 - Event 1</a>
-				<a href="downloads/season14/baps-season14-points.pdf" target="_blank">Season 14 Points</a>
-				<a href="http://eepurl.com/n5HGH" target="_blank">Subscribe to BAPS</a>
-			</nav>
-		</section>
-	</header>
-	<section id="top">
-		<section class="page clearfix">
-			<br />
-			<br />
-			<img src="images/season-14/season14event1.png" />
-			<h1>Congratulations to Season 14, Event #1 Winners</h1>
-			<p>
-				<div class="left schedule">
-					1st - Ty Kovacs
-					<br />
-					2nd - Adam Jarashow
-					<br />
-					3rd - Jerry Grigsby
-					<br />
-					4th - Daniel Gough
-					<br />
-					5th - Jeremy Beaver
-					<br />
-					6th - Pam Siebert
-					<br />
-					7th - David Smario
-					<br />
-					8th - Clark Brooke
-					<br />
-					9th - Tan Nguyen
-				</div>
-                Total players: 37
-			</p>
-			<br />
-			<br />
-		</section>
-	</section>
-	<section id="bottom">
-		<section class="page clearfix">
-			<br />
-			<br />
-				<h1>Get ready for Season 14</h1>
-				<h2>Event #2 on Saturday, September 17, 2022!</h2>
-                <p>
-                    &bullet; Doors will open at 3:30PM for registration.
-    				<br />
-                    &bullet; There will be $10,000 chips for <b>ALL PLAYERS</b> between 3:30PM and 4:00PM.
-    				<br />
-                    &bullet; There will be $9,000 chips for prepaid players after 4:00PM, up to 3 rounds. If no show after 3 rounds, the chips will be taken away.
-    				<br />
-                    &bullet; Cards will be dealt at 4:00PM sharp.
-    				<br />
-                    &bullet; $40 entry fee.
-    				<br />
-                    &bullet; NO CASH! Cash App required. Pay by Cash App at the door. No other options. Required by FOG.
-    				<br />
-                    &bullet; BAPS Event must be done and cleared out of FOG by 9:45PM
-    				<br /><br />
-                    <b>Location for BAPS for Season 14</b>
-    				<br />
-                    Fremont Oak Gardens (FOG)
-                    <br />
-                    2681 Driscoll Road, Fremont, CA
-                    <br />
-                    <a href="https://www.google.com/maps/preview?q=2681+Driscoll+Road,+Fremont,+CA" target="_blank">MAP</a>
-    				<br /><br />
-                    **DO NOT bring your chair. There are plenty of chairs at FOG.**
-                    <br />
-                    **Regarding COVID-19, no requirements (mask, 6 ft, etc) but you are at your own risk.**
-                </p>
-			<br />
-		</section>
-	</section>
-	<section id="top">
-		<section class="page clearfix">
+                <a href="downloads/season14/baps-season14-event3.pdf" target="_blank">Season 14 - Event 3</a>
+                <a href="downloads/season14/baps-season14-points.pdf" target="_blank">Season 14 Points</a>
+                <a href="http://eepurl.com/n5HGH" target="_blank">Subscribe to BAPS</a>
+            </nav>
+        </section>
+    </header>
+    <section id="top">
+        <section class="page clearfix">
+            <br />
+            <br />
+            <img src="images/season-14/season14event3.png" />
+            <h1>Congratulations to Season 14, Event #3 Winners</h1>
+            <p>
+            <div class="left schedule">
+                1st - Mike Sidansky
+                <br />
+                2nd - Valeriy Kutsov
+                <br />
+                3rd - Tristan Boren
+                <br />
+                4th - Clark Brooke
+                <br />
+                5th - Mikhail Chirkin
+                <br />
+                6th - Pam Siebert
+                <br />
+                7th - David Smario
+                <br />
+                8th - Julia Hong
+                <br />
+                9th - Ryan Biser
+                <br />
+                10th - Joey Baer
+                <br />
+                11th - Joshua Weinstock
+                <br />
+                12th - Hyun Cho
+                <br />
+                13th - Brotrae Bullock
+                <br />
+                14th - Mark Farr
+                <br />
+                15th - Hendri Argo
+            </div>
+            Total players: 40
+            </p>
+            <br />
+            <br />
+        </section>
+    </section>
+    <section id="bottom">
+        <section class="page clearfix">
+            <br />
+            <br />
+            <img src="images/season-14/season14event2.png" />
+            <h1>Congratulations to Season 14, Event #2 Winners</h1>
+            <p>
+            <div class="left schedule">
+                1st - Sulghi Hong
+                <br />
+                2nd - Elizabeth Grigsby
+                <br />
+                3rd - Mikhail Chirkin
+                <br />
+                4th - David Harvey
+                <br />
+                5th - Tan Nguyen
+                <br />
+                6th - Mark Farr
+                <br />
+                7th - Tristan Boren
+                <br />
+                8th - Ryan Biser
+                <br />
+                9th - Pat Woo
+            </div>
+            Total players: 38
+            </p>
+            <br />
+            <br />
+        </section>
+    </section>
+    <section id="top">
+        <section class="page clearfix">
+            <br />
+            <br />
+            <img src="images/season-14/season14event1.png" />
+            <h1>Congratulations to Season 14, Event #1 Winners</h1>
+            <p>
+            <div class="left schedule">
+                1st - Ty Kovacs
+                <br />
+                2nd - Adam Jarashow
+                <br />
+                3rd - Jerry Grigsby
+                <br />
+                4th - Daniel Gough
+                <br />
+                5th - Jeremy Beaver
+                <br />
+                6th - Pam Siebert
+                <br />
+                7th - David Smario
+                <br />
+                8th - Clark Brooke
+                <br />
+                9th - Tan Nguyen
+            </div>
+            Total players: 37
+            </p>
+            <br />
+            <br />
+        </section>
+    </section>
+    <section id="bottom">
+        <section class="page clearfix">
+            <br />
+            <br />
+            <h1>Season 14</h1>
+            <p>
+                &bullet; Doors will open at 3:30PM for registration.
+                <br />
+                &bullet; There will be $10,000 chips for <b>ALL PLAYERS</b> between 3:30PM and 4:00PM.
+                <br />
+                &bullet; There will be $9,000 chips for prepaid players after 4:00PM, up to 3 rounds. If no show after 3
+                rounds, the chips will be taken away.
+                <br />
+                &bullet; Cards will be dealt at 4:00PM sharp.
+                <br />
+                &bullet; $40 entry fee.
+                <br />
+                &bullet; NO CASH! Cash App required. Pay by Cash App at the door. No other options. Required by FOG.
+                <br />
+                &bullet; BAPS Event must be done and cleared out of FOG by 9:45PM
+                <br /><br />
+                <b>Location for BAPS for Season 14</b>
+                <br />
+                Fremont Oak Gardens (FOG)
+                <br />
+                2681 Driscoll Road, Fremont, CA
+                <br />
+                <a href="https://www.google.com/maps/preview?q=2681+Driscoll+Road,+Fremont,+CA" target="_blank">MAP</a>
+                <br /><br />
+                **DO NOT bring your chair. There are plenty of chairs at FOG.**
+                <br />
+                **Regarding COVID-19, no requirements (mask, 6 ft, etc) but you are at your own risk.**
+            </p>
+            <br />
+        </section>
+    </section>
+    <section id="top">
+        <section class="page clearfix">
             <br>
-			<h1>BAPS - Season 14</h1>
+            <h1>BAPS - Season 14</h1>
             <h2>Tournament Schedule - August 2022 to June 2023</h2>
             <br>
-            <span class="completed">Event 1 – August 20, 2022 (FOG approved)</span> <!-- if completed, enter class="completed" -->
+            <span class="completed">Event 1 – August 20, 2022 (FOG approved)</span>
+            <!-- if completed, enter class="completed" -->
             <br>
-            <span class="">Event 2 - September 17, 2022***</span>
+            <span class="completed">Event 2 - September 17, 2022 (FOG approved)</span>
             <br>
-            <span class="">Event 3 - October 8, 2022***</span>
+            <span class="completed">Event 3 - SFDC - October 8, 2022 (FOG approved)</span>
             <br>
             <span class="">Event 4 - November 12, 2022***</span>
             <br>
@@ -122,40 +201,42 @@
             <span class="">Note *** - pending FOG approval. Will announce as soon as dates are approved.</span>
             <br />
             <br />
-		</section>
-	</section>
-	<section id="bottom">
-		<section class="page clearfix">
-			<div class="box 1">
-				<h2>Welcome</h2>
+        </section>
+    </section>
+    <section id="bottom">
+        <section class="page clearfix">
+            <div class="box 1">
+                <h2>Welcome</h2>
 
-				<p>
-					BAPS is organized exclusively for the Deaf poker community and is open to any player in the ASL community who would like to participate.
-				</p>
-				<p>
-					You can like us on our facebook page and/or subscribe to our annoucements.
-				</p>
+                <p>
+                    BAPS is organized exclusively for the Deaf poker community and is open to any player in the ASL
+                    community who would like to participate.
+                </p>
+                <p>
+                    You can like us on our facebook page and/or subscribe to our annoucements.
+                </p>
                 <br />
                 <br />
 
-			</div>
-			<div class="box 2">
-				<h2>News</h2>
-				<p>
-					BAPS is now organized by a committee: Elizabeth Grigsby, David Seto, David Smario, Eric Kunze, Jaime Jackerson, Jerry Grigsby
-				</p>
-				<p>Good luck!</p>
+            </div>
+            <div class="box 2">
+                <h2>News</h2>
+                <p>
+                    BAPS is now organized by a committee: Elizabeth Grigsby, David Seto, David Smario, Eric Kunze, Jaime
+                    Jackerson, Jerry Grigsby
+                </p>
+                <p>Good luck!</p>
                 <br>
                 <br>
-			</div>
-			<div class="box 3 small">
-				<h2>Tournament Schedule</h2>
-				<h2>Season 14, 2022-23</h2>
+            </div>
+            <div class="box 3 small">
+                <h2>Tournament Schedule</h2>
+                <h2>Season 14, 2022-23</h2>
                 <span class="completed">Event 1 – August 20, 2022</span> <!-- if completed, enter class="completed" -->
                 <br>
-                <span class="">Event 2 - September 17, 2022***</span>
+                <span class="completed">Event 2 - September 17, 2022</span>
                 <br>
-                <span class="">Event 3 - October 8, 2022***</span>
+                <span class="completed">Event 3 - SFDC - October 8, 2022</span>
                 <br>
                 <span class="">Event 4 - November 12, 2022***</span>
                 <br>
@@ -168,27 +249,30 @@
                 <span class="">Event 8 – May 13, 2023***</span>
                 <br>
                 <span class="">Free Roll – June 10, 2023***</span>
-				<br>
+                <br>
                 <span class="">Note *** - pending FOG approval. Will announce as soon as dates are approved.</span>
                 <br>
-			</div>
+            </div>
 
-		</section>
+        </section>
 
-	</section>
+    </section>
 
 
-	<footer>
-		<section class="page" style="padding-top: 20px;">
+    <footer>
+        <section class="page" style="padding-top: 20px;">
 
-			<nav style="width: 100%; margin: 0px auto; text-align: center; padding-bottom: 0px;">
-				<a href="info.php" target="_blank">BAPS Info</a>
-			</nav>
+            <nav style="width: 100%; margin: 0px auto; text-align: center; padding-bottom: 0px;">
+                <a href="info.php" target="_blank">BAPS Info</a>
+            </nav>
             <p style="clear:both;">&nbsp;</p>
-			<div>&copy; Deaf Bay Area Poker Series - 2013&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/groups/bayareapokerseries/" target="_blank"><img src="images/Facebook_icon.png" border="0" width="25" /></a></div>
-			<br />
-		</section>
-	</footer>
+            <div>&copy; Deaf Bay Area Poker Series - 2013&nbsp;&nbsp;&nbsp;<a
+                    href="https://www.facebook.com/groups/bayareapokerseries/" target="_blank"><img
+                        src="images/Facebook_icon.png" border="0" width="25" /></a></div>
+            <br />
+        </section>
+    </footer>
 
 </body>
+
 </html>
